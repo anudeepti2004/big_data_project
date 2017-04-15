8d7142ae333ee233fa96413400588ebd9f9962ea
Utku Evci <ue225@login-1-1.local>
2017-04-15 19:34:04 -0400

    converted scripts

--- scripts/mta_tax.py
+++ scripts/mta_tax.py
@@ -7,26 +7,26 @@ from csv import reader
 from myUtils import readFiles,getSomeFileNames
 
 if __name__ == "__main__":
-	sc = SparkContext()
+    sc = SparkContext()
     fileNames = getSomeFileNames({2016:range(1,7)})
-	(taxi_data,prefix) = readFiles(fileNames,sc )
-	def checkValid(mta_tax):
-		try:
-			num = float(mta_tax)
-			if  num > 0:
-				return "Valid"
-			elif num == 0:
-				return "Valid_ZeroTax"
-			else:
-				return "Invalid_NegativeMtaTax"
-		except ValueError:
-			return "Invalid_NotFloat"
-		
-	field = taxi_data.map(lambda entry: (checkValid(entry[14]),1)).reduceByKey(lambda x,y: x+y)
-	
-	tabSeparated =  field.map(lambda x: x[0]+"\t"+str(x[1])) 
-    	tabSeparated.saveAsTextFile("mta_info.out")
-	
-	sc.stop()
-	
+    (taxi_data,prefix) = readFiles(fileNames,sc )
+    def checkValid(mta_tax):
+        try:
+            num = float(mta_tax)
+            if  num > 0:
+                return "Valid"
+            elif num == 0:
+                return "Valid_ZeroTax"
+            else:
+                return "Invalid_NegativeMtaTax"
+        except ValueError:
+            return "Invalid_NotFloat"
+        
+    field = taxi_data.map(lambda entry: (checkValid(entry[14]),1)).reduceByKey(lambda x,y: x+y)
+    
+    tabSeparated =  field.map(lambda x: x[0]+"\t"+str(x[1])) 
+    tabSeparated.saveAsTextFile("mta_info.out")
+    
+    sc.stop()
+    
 
--- scripts/vendorId_invalid.py
+++ scripts/vendorId_invalid.py
@@ -4,26 +4,28 @@ import sys
 from operator import add
 from pyspark import SparkContext
 from csv import reader
-from myUtils import readFiles
+from myUtils import readFiles,getSomeFileNames
 
 if __name__ == "__main__":
-	sc = SparkContext()
-	(taxi_data,prefix) = readFiles(['data/yellow_tripdata_2016-01.csv','data/yellow_tripdata_2016-02.csv','data/yellow_tripdata_2016-03.csv','data/yellow_tripdata_2016-04.csv','data/yellow_tripdata_2016-05.csv','data/yellow_tripdata_2016-06.csv'],sc )
-	def checkValid(vendor_id):
-		try:
-			int(vendor_id)
-			if int(vendor_id) == 1 or int(vendor_id) == 2:
-				return "Valid"
-			else:
-				return "Invalid"
-		except ValueError:
-			return "Invalid"
-		
-	vendorID = taxi_data.map(lambda entry: (checkValid(entry[0]),1)).reduceByKey(lambda x,y: x+y)
-	
-	tabSeparated =  vendorID.map(lambda x: x[0]+"\t"+str(x[1])) 
-    	tabSeparated.saveAsTextFile("vendorId_valid.out")
-	
-	sc.stop()
-	
+    sc = SparkContext()
+    fileNames = getSomeFileNames({2016:range(1,7)})
+    print(fileNames)
+    (taxi_data,prefix) = readFiles(fileNames,sc )
+    def checkValid(vendor_id):
+        try:
+            int(vendor_id)
+            if int(vendor_id) == 1 or int(vendor_id) == 2:
+                return "Valid"
+            else:
+                return "Invalid"
+        except ValueError:
+            return "Invalid"
+        
+    vendorID = taxi_data.map(lambda entry: (checkValid(entry[0]),1)).reduceByKey(lambda x,y: x+y)
+    
+    tabSeparated =  vendorID.map(lambda x: x[0]+"\t"+str(x[1])) 
+    tabSeparated.saveAsTextFile("vendorId_valid.out")
+    
+    sc.stop()
+    
 
