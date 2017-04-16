e1b3515a60d361ab03df3025bb63bbddc68212b2
evcu <evcu@users.noreply.github.com>
2017-04-16 16:53:54 -0400

    new reader

--- scripts/myUtils.py
+++ scripts/myUtils.py
@@ -4,22 +4,54 @@ import sys
 from operator import add
 from csv import reader
 
-def readFiles (files,sc):
+def readAllFiles (sc):
+    cy_m_dic = dict((y, [k for k in range(1,13)]) for y in range(2013,2017))
+    return readFiles(y_m_dic,sc)
+
+OUR_DATABASE_PATH = '/user/dv697/data/yellow_tripdata_'
+
+def readFiles (year_months_dic,sc):
+    basePath = OUR_DATABASE_PATH
     concatenatedFiles = ','.join(files)
-    
-    csvfile = sc.textFile(concatenatedFiles)
+    new_type = []
+    old_type = []
+    for y,m_array in year_months_dic.items():
+        for m in m_array:
+            # No gps 
+            if y == 2016 and m > 6:
+                new_type.append(basePath + '%d-%02d.csv' %(y,m))
+            else:
+                old_type.append(basePath + '%d-%02d.csv' %(y,m))
+
+    oldTypeFiles = ','.join(old_type)
+    newTypeFiles = ','.join(new_type)
+
+
+    csvfile = sc.textFile(oldTypeFiles)
     header = csvfile.first()
 
     csvfile = csvfile.filter(lambda line : line != header)
     # taxi_data.map(lambda t: map(float,t[5:7]))
     
     taxi_data = csvfile.mapPartitions(lambda x: reader(x))
-    if "yellow" in concatenatedFiles:
+
+    csvfile2 = sc.textFile(newTypeFiles)
+    header2 = csvfile2.first()
+
+    csvfile2 = csvfile2.filter(lambda line : line != header2)
+    # taxi_data.map(lambda t: map(float,t[5:7]))
+    
+    ## Assign GPS coordinates to each place
+    taxi_data2 = csvfile2.mapPartitions(lambda x: reader(x)).map(lambda a: a[:5] + [0.0, 0.0] + a[5:7] + [0.0, 0.0] + a[9:])
+
+    taxi_data.union(taxi_data2)
+
+    if "yellow" in oldTypeFiles + newTypeFiles:
         return (taxi_data,"yellow")
     else:
         return (taxi_data,"green")
+
     
-OUR_DATABASE_PATH = '/user/dv697/data/yellow_tripdata_'
 def getAllFileNames():
     y_m_dic = dict((y, [k for k in range(1,13)]) for y in range(2013,2017))
     return getSomeFileNames(y_m_dic)
