4884b682a486b9e43251fd560069015c383200e8
Utku Evci <ue225@login-1-1.local>
2017-04-15 21:03:09 -0400

    there is a mistake at naming here

---
+++ scripts/dropoff_latitude.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+import sys
+from pyspark import SparkContext
+from csv import reader
+from myUtils import readFiles,getSomeFileNames
+
+if __name__ == "__main__":
+    sc = SparkContext()
+    fileNames = getSomeFileNames({2016:range(1,7)})
+    (taxi_data,prefix) = readFiles(fileNames,sc )
+	def checkValid(f,range):
+	    try:
+	        ff = float(f)
+	        if  range[1] >ff> range[0]:
+	            return "Valid"
+	        else:
+	            return "Invalid_NotNYC"
+	    except ValueError:
+	        return "Invalid_NotFloat"
+
+	lat = (40.477399,40.917577)  
+    field = taxi_data.map(lambda entry: (checkValid(entry[6],lat),1)).reduceByKey(lambda x,y: x+y)
+    
+    tabSeparated =  field.map(lambda x: x[0]+"\t"+str(x[1])) 
+    tabSeparated.saveAsTextFile("mta_info.out")
+    
+    sc.stop()
---
+++ scripts/dropoff_longitude.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+import sys
+from pyspark import SparkContext
+from csv import reader
+from myUtils import readFiles,getSomeFileNames
+
+if __name__ == "__main__":
+    sc = SparkContext()
+    fileNames = getSomeFileNames({2016:range(1,7)})
+    (taxi_data,prefix) = readFiles(fileNames,sc )
+    def checkValid(f,range):
+        try:
+            ff = float(f)
+            if  range[1] > ff > range[0]:
+                return "Valid"
+            else:
+                return "Invalid_NotNYC"
+        except ValueError:
+            return "Invalid_NotFloat"
+
+    lon = (-74.259090,-73.700272)    
+    field = taxi_data.map(lambda entry: (checkValid(entry[5],lon),1)).reduceByKey(lambda x,y: x+y)
+    
+    tabSeparated =  field.map(lambda x: x[0]+"\t"+str(x[1])) 
+    tabSeparated.saveAsTextFile("dropoff_long.out")
+    
+    sc.stop()
