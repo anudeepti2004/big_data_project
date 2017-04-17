6d15b3ce7767a0bdbf3a1410ce10e2f853d69e10
Utku Evci <ue225@login-1-1.local>
2017-04-16 23:50:58 -0400

    a

--- scripts/myUtils.py
+++ scripts/myUtils.py
@@ -48,11 +48,11 @@ def readFiles2 (year_months_dic,sc):
 
     oldTypeFiles = ','.join(old_type)
     newTypeFiles = ','.join(new_type)
-
+    print(oldTypeFiles,newTypeFiles)
     if oldTypeFiles:
         csvfile = sc.textFile(oldTypeFiles)
         header = csvfile.first()
-
+	print(header)
         csvfile = csvfile.filter(lambda line : line != header)
         # taxi_data.map(lambda t: map(float,t[5:7]))
     
@@ -60,23 +60,23 @@ def readFiles2 (year_months_dic,sc):
     else:
         csvfile = sc.textFile(newTypeFiles)
         header = csvfile.first()
-
+	print(header)
         csvfile = csvfile.filter(lambda line : line != header)
         # taxi_data.map(lambda t: map(float,t[5:7]))
     
         taxi_data = csvfile.mapPartitions(lambda x: reader(x))
-        zones_mean = pickle.load(open('scripts/zones_mean.pickle','r'))
+        zones_mean = pickle.load(open('zones_mean.pickle','r'))
         taxi_data = csvfile.mapPartitions(lambda x: reader(x)).map(lambda a: a[:5] + zones_mean[int(a[7])] + a[5:7] + zones_mean[int(a[8])] + a[9:])
 
     if oldTypeFiles and newTypeFiles:
         csvfile2 = sc.textFile(newTypeFiles)
         header2 = csvfile2.first()
-
+	print(header2)
         csvfile2 = csvfile2.filter(lambda line : line != header2)
         # taxi_data.map(lambda t: map(float,t[5:7]))
         
         ## Assign GPS coordinates to each place
-        zones_mean = pickle.load(open('scripts/zones_mean.pickle','r'))
+        zones_mean = pickle.load(open('zones_mean.pickle','r'))
         taxi_data2 = csvfile2.mapPartitions(lambda x: reader(x)).map(lambda a: a[:5] + zones_mean[int(a[7])] + a[5:7] + zones_mean[int(a[8])] + a[9:])
         taxi_data.union(taxi_data2)
 
