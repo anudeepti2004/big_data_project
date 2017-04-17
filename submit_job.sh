597cf6461000080ae6b5239ca83b7ed2415c9e60
woollysocks <nikitanangia@gmail.com>
2017-04-16 23:25:33 -0400

    getmerge not get.

--- submit_job.sh
+++ submit_job.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash 
 
 # Make sure we have access to the correct lbibraries
-module load pandas/0.18.1
+module load pandas/python2.7/0.18.1
 
 
 array=(VendorID tpep_dropoff_datetime passenger_count trip_distance pickup_longitude pickup_latitude store_and_fwd_flag dropoff_longitude dropoff_latitude payment_type fare_amount extra mta_tax tip_amount tolls_amount improvement_surcharge total_amount tpep_pickup_datetime RatecodeID)
@@ -12,5 +12,5 @@ spark-submit  validate.py
 # Get output files
 for i in "${array[@]}"
 do
-	hadoop fs -get "$i"_valid.out "$i"_valid.out
+	hadoop fs -getmerge "$i"_valid.out "$i"_valid.out
 done
