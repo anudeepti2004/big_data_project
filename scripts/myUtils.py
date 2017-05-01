43bc8aa566120cae97841ca7c5b680e9eca01e56
evcu <evcu@users.noreply.github.com>
2017-05-01 15:39:49 -0400

    fixed syntax

--- scripts/myUtils.py
+++ scripts/myUtils.py
@@ -157,7 +157,7 @@ def cleanByFields(data,fields):
     #fields: array of strings ['pickup_longitude','pickup_latitude']
     t=data
     for f in fields:
-        i = my._fieldsDic[f]
+        i = _fieldsDic[f]
         t = t.filter(lambda e: va.checkLongitude(e[i]).startswith('Valid'))
     return t
 
