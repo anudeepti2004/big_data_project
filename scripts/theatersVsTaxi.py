b6aa85f9659b5c8086e7a8d31ffc2fadc0718c76
evcu <evcu@users.noreply.github.com>
2017-05-01 15:38:07 -0400

    fixed syntax

--- scripts/theatersVsTaxi.py
+++ scripts/theatersVsTaxi.py
@@ -1,12 +1,11 @@
 import myUtils as my
 import spatialUtils as sp
+sc.addPyFile("myUtils.py")
+sc.addPyFile("validation_utils.py")
+sc.addPyFile("spatialUtils.py")
+
 data,flag = my.readAllFiles(sc)
-
-my._fieldsDic['pickup_longitude']
-
 theaters = sp.loadNYCtheaters()
-
-
 LON_LIM = 0.002
 LAT_LIM = 0.0012 
 ven = theaters['Broadway Theatre']
