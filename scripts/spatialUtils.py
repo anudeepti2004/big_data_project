63948f304044f4d878850e7b39e279a0ada74b65
evcu <evcu@users.noreply.github.com>
2017-05-01 11:26:07 -0400

    splitting utils

---
+++ scripts/spatialUtils.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+import sys
+
+
+def loadNYCtheaters():
+	theaters = {}
+	with open('theaters.csv','r') as f:
+	    f.next()
+	    for l in f: 
+	        aa = l.split(',')
+	        coor = map(float,filter(lambda x: len(x)>0,re.split('[^\d.-]+',aa[0])))
+	        name = aa[1]
+	        theaters[name] = coor
+
+	return theaters
+
+def createBox(c_tuple,x_lim,y_lim):
+# tuple of floats c_tuple,new_point as (lon,lat)
+    box = ((c_tuple[0]-x_lim,c_tuple[1]-y_lim),(c_tuple[0]+x_lim,c_tuple[1]+y_lim))
+    def filterBox(dd):
+            for i in range(len(dd)):
+                if not(box[0][i]<dd[i]<box[1][i]):
+                    return False
+            return True
+    return filterBox
+
+def filterTheBox(data,filterBox,c_lon,c_lat):
+    p_lon = my._fieldsDic[c_lon]
+    p_lat = my._fieldsDic[c_lat]
+    pickups = data.map(lambda a: map(float,[a[p_lon],a[p_lat]]))
+    return pickups.filter(lambda a: filterBox(a))
