2fbe283cf0d3cb1c97b22b327e88cc52ce2c5963
anudeepti2004 <anudeepti2004@gmail.com>
2017-05-04 23:29:45 -0400

    Fixed a bug and added more memory

--- scripts/correlation_between_fare_amount_tip_amount.py
+++ scripts/correlation_between_fare_amount_tip_amount.py
@@ -21,16 +21,18 @@ fields = getFieldDic()
 (taxi_data,prefix) = readAllFiles(sc)
 
 taxi_data = cleanByFields(taxi_data, ['tip_amount','fare_amount'])
-taxi_data_required_field = taxi_data.map(lambda entry: (float(entry[fields['tip_amount']]),float(entry[fields['fare_amount']]))).filter(lambda x: x[0] > 0 and x[1] > 0).map(lambda x: ((x[0],x[1]),1)).reduceByKey(lambda x,y : x+y)
+taxi_data_required_field = taxi_data.map(lambda entry: (float(entry[fields['tip_amount']]),float(entry[fields['fare_amount']]))).filter(lambda x: x[0] > 0 and x[1] > 0 and x[0] <= x[1])#.map(lambda x: ((x[0],x[1]),1)).reduceByKey(lambda x,y : x+y)
 
 joined_rdd = taxi_data_required_field.sortByKey()
 
-rdd1_values = joined_rdd.map(lambda x:x[0][0])
-rdd2_values = joined_rdd.map(lambda x:x[0][1])
+#rdd1_values = joined_rdd.map(lambda x:x[0][0])
+#rdd2_values = joined_rdd.map(lambda x:x[0][1])
+rdd1_values = joined_rdd.map(lambda x:x[0])
+rdd2_values = joined_rdd.map(lambda x:x[1])
 correlation_value = Statistics.corr(rdd1_values, rdd2_values)
 
 f = open( 'correlation_output_tip_amount_vs_fare_amount.out', 'w' )
 f.write( 'Correlation value of fare amount and tip amount = ' + str(correlation_value) + '\n' )
 
-tabSeparated =  joined_rdd.map(lambda x: str(x[0][0])+"\t"+str(x[0][1])+"\t"+str(x[1])) 
+tabSeparated =  joined_rdd.map(lambda x: ((x[0],x[1]),1)).reduceByKey(lambda x,y : x+y).map(lambda x: str(x[0][0])+"\t"+str(x[0][1])+"\t"+str(x[1])) 
 tabSeparated.saveAsTextFile("tip_amount_fare_amount.out")
--- scripts/run_correlation_between_fare_amount_tip_amount.sh
+++ scripts/run_correlation_between_fare_amount_tip_amount.sh
@@ -8,6 +8,6 @@ export SPARK_YARN_USER_ENV=PYTHONHASHSEED=0
 
 hadoop fs -rm -r "tip_amount_fare_amount.out"
 # Run
-spark-submit correlation_between_fare_amount_tip_amount.py > log
+spark-submit --driver-memory 3g --executor-memory 3g correlation_between_fare_amount_tip_amount.py > log
 
 hadoop fs -getmerge "tip_amount_fare_amount.out" "tip_amount_fare_amount.out"
