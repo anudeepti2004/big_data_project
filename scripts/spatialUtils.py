3868087bb94b9ebcdda83f85ec3ace415ba05ea4
evcu <evcu@users.noreply.github.com>
2017-05-08 22:42:38 -0400

    new fun spatial union of filters

--- scripts/spatialUtils.py
+++ scripts/spatialUtils.py
@@ -30,3 +30,20 @@ def filterTheBox(data,filterBox,c_lon,c_lat):
     p_lat = myUtils._fieldsDic[c_lat]
     filter_fun=lambda a: filterBox(map(float,[a[p_lon],a[p_lat]]))
     return data.filter(filter_fun)
+
+def createUnionOfBoxes(c_tuples,x_lim,y_lim):
+# tuple of floats c_tuple,new_point as (lon,lat)
+    def genericfilterBox(dd,box):
+        for i in range(len(dd)):
+            if not(box[0][i]<dd[i]<box[1][i]):
+                return False
+        return True
+    b_array = []
+    for c_tuple in  c_tuples:
+        b_array.append(((c_tuple[0]-x_lim,c_tuple[1]-y_lim),(c_tuple[0]+x_lim,c_tuple[1]+y_lim)))
+    def unionFun(dd):
+        for b in b_array:
+            if genericfilterBox(dd,b):
+                return True
+        return False
+    return unionFun
