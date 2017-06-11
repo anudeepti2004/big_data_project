c7802a978a5908782d5ecdf5beb11df3209c165f
evcu <evcu@users.noreply.github.com>
2017-06-11 12:50:04 -0700

    Cleaning local repo

--- scripts/month_coordinates.py
+++ scripts/month_coordinates.py
@@ -12,7 +12,7 @@ coordinates = pickup_cleaned.map(lambda a: (my.readAtt(a,'pickup_longitude'), my
 coordinates.saveAsTextFile("2015_8_coor.out")
 
 
-data,flag =my.readFiles2(dict((y, [k for k in [10]]) for y in range(2015,2016)),sc)
+data,flag =my.readFiles2(dict((y, [k for k in [1]]) for y in range(2015,2016)),sc)
 pickup_cleaned = my.cleanByFields(data,['pickup_longitude','pickup_latitude'])
 coordinates = pickup_cleaned.map(lambda a: (my.readAtt(a,'pickup_longitude'), my.readAtt(a,'pickup_latitude')))
-coordinates.saveAsTextFile("2015_10_coor.out")
+coordinates.saveAsTextFile("2015_1_coor.out")
